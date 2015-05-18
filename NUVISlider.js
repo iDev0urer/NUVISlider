00467b85f654840812813843ba6f9fb17aca2ccb
Chris Watson <chris@Chriss-MacBook-Pro.local>
2015-05-18 12:37:06 -0600

    Added pause on hover funcitonality

--- NUVISlider.js
+++ NUVISlider.js
@@ -11,11 +11,14 @@
             defaults = {
                 slideElement    : '.slide',
                 anchor          : '.slide-link',
+                activeClass     : 'active',
                 elementsToShow  : 1,
                 automatic       : false,
+                pauseOnHover    : true,
+                timing          : 5000,
                 slideTransition : {
-                    transition  : 'linear',
-                    duration    : '.5s'
+                    easing      : 'easeIn',
+                    duration    : 1500
                 }
             };
 
@@ -37,18 +40,32 @@
 
         Plugin.prototype.init = function () {
 
-        var options, self, slide, slides, slideCount, anchors, anchorCount, href, thisSlide, pos, index,i, firstEl, width, currentSlide;
+        var options, self, slide, slides, slideCount, anchors, anchorCount, href, thisSlide, pos, index,i, firstEl, width, currentSlide, paused;
 
         options = this.options;
         self = this.element;
         slides = $(self).find(options.slideElement);
         slideCount = slides.length;
+        sections = slideCount/options.elementsToShow;
         anchors = $('body').find(options.anchor);
         anchorCount = anchors.length;
         width = $('body').width();
             
         function init() {
             position_slides();
+            $(options.anchor + ':first-child').addClass(options.activeClass);
+            
+            if (options.automatic == true) {
+                paused = false;
+                playSlider();
+            }
+            
+            if (options.pauseOnHover == true) {
+                $(options.slideElement).parent().on("mouseenter", function() { paused = true; console.log('paused'); } );
+                $(options.slideElement).parent().on("mouseleave", function() { paused = false; console.log('playing'); } );
+            }
+            
+            console.log($(options.slideElement).parent());
         }
             
         function position_slides() {
@@ -57,9 +74,9 @@
                 slide = slides[i];
                 
                 if (i!=0) {
-                    $(slide).css( 'left', width );
+                    $(slide).css({ left: width, opacity: 0 }).removeClass(options.activeClass);
                 } else {
-                    $(slide).css( 'left', '0px' );
+                    $(slide).css( 'left', '0px' ).addClass(options.activeClass);
                 }
             }
             
@@ -68,13 +85,45 @@
         $(options.anchor).on('click', function(e) {
             e.preventDefault();
             href = $(this).attr('href');
-            // window.location.hash = href;
+            href = $(href).index();
+            
+            $(this).addClass(options.activeClass);
             slideTo(href);
         });
             
-        function slideTo(element) {
-            nextSlide = $(element);
+        function playSlider() {
+            timing = options.timing;
+                setTimeout (function() {
+                    if (paused == false) {
+                        slideToNext();                
+                    }
+                    playSlider();
+                }, timing);
+        }
+            
+        function slideToNext() {
+            currentSlide = $(options.slideElement + '.'+options.activeClass).index();
+            var nextSlide = currentSlide + 1;
+            if (nextSlide < slideCount) {
+                nextSlide = currentSlide + 1;
+            } else {
+                nextSlide = 0;
+            }
+            
+            slideTo(nextSlide);
+        };
+            
+        function slideTo(index) {
+            nextSlide = $(slides[index]);
             pos = nextSlide.css('left');
+            currentAnchor = $('a[href="#' + nextSlide.attr('id') + '"]');
+            
+            
+            $.each($(options.anchor), function(i,el) {
+                $(el).removeClass(options.activeClass);
+            });
+            
+            currentAnchor.addClass(options.activeClass);
             
             for  (i=0; i<slideCount; i++) {
                 
@@ -82,11 +131,11 @@
 
                 if ( i == nextSlide.index() ) {
                     
-                    $(slide).animate({ left: '0px' });
+                    $(slide).css({ opacity: 1 }).animate({ left: '0px' }, options.slideTransition.duration).addClass(options.activeClass);
                     
                 } else {
-                    $(slides[i]).animate({ left: -width, opacity: 0 }, 500, function(e) {
-                        $(this).css({ left: width, opacity: 1 });
+                    $(slides[i]).animate({ left: -width }, options.slideTransition.duration, function(e) {
+                        $(this).css({ left: width, opacity: 0 }).removeClass(options.activeClass);
                     });
                 }
             }
